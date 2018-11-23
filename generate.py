8f723e1c1e0482e93d78a369d228e6be37bedfef
Mark Nottingham <mnot@mnot.net>
2018-11-23 11:30:17 +1100

    check first characters.

--- generate.py
+++ generate.py
@@ -54,6 +54,21 @@ for c in range(0x00, 0x7f + 1):
     else:
         test["must_fail"] = True
     tests.append(test)
+## allowed starting characters
+allowed_identifier_start_chars = list(range(0x61, 0x7a + 1))
+for c in range(0x00, 0x7f + 1):
+    test = {
+      "name": "0x%02x starting an identifier" % c,
+      "raw": ["%sa" % chr(c)],
+      "header_type": "item"
+    }
+    if c in [0x09, 0x20]:
+        test["expected"] = "a"  # whitespace is always stripped.
+    elif c in allowed_identifier_start_chars:
+        test["expected"] = "%sa" % chr(c)
+    else:
+        test["must_fail"] = True
+    tests.append(test)
 write('identifier', tests)
 
 ### keys
@@ -61,6 +76,7 @@ tests = []
 ## allowed characters
 allowed_key_chars = list(range(0x30, 0x39 + 1)) + list(range(0x61, 0x7a + 1)) + \
     [ord(c) for c in ["_", "-"]]
+allowed_key_start_chars = list(range(0x61, 0x7a + 1))
 ## dictionary keys
 for c in range(0x00, 0x7f + 1):
     test = {
@@ -74,6 +90,20 @@ for c in range(0x00, 0x7f + 1):
     else:
         test["must_fail"] = True
     tests.append(test)
+## allowed dictionary key starting characters
+for c in range(0x00, 0x7f + 1):
+    test = {
+      "name": "0x%02x starting an dictionary key" % c,
+      "raw": ["%sa=1" % chr(c)],
+      "header_type": "dictionary"
+    }
+    if c in [0x09, 0x20]:
+        test["expected"] = {"a": 1}  # whitespace is always stripped.
+    elif c in allowed_identifier_start_chars:
+        test["expected"] = {"%sa" % chr(c): 1}
+    else:
+        test["must_fail"] = True
+    tests.append(test)
 ## param-list keys
 for c in range(0x00, 0x7f + 1):
     test = {
@@ -84,6 +114,20 @@ for c in range(0x00, 0x7f + 1):
     if c in allowed_key_chars:
         key = "a%sa" % chr(c)
         test["expected"] = [["foo", {key: 1}]]
+    else:
+        test["must_fail"] = True
+    tests.append(test)
+## allowed param-list key starting characters
+for c in range(0x00, 0x7f + 1):
+    test = {
+      "name": "0x%02x starting a param-list key" % c,
+      "raw": ["foo; %sa=1" % chr(c)],
+      "header_type": "param-list"
+    }
+    if c in [0x09, 0x20]:
+        test["expected"] = [["foo", {"a": 1}]]  # whitespace is always stripped.
+    elif c in allowed_identifier_start_chars:
+        test["expected"] = [["foo", {"%sa" % chr(c): 1}]]
     else:
         test["must_fail"] = True
     tests.append(test)
