feea1f7e94246eb295b38e00af33ec990b63ca85
Jxck <block.rxckin.beats@gmail.com>
2021-02-06 18:05:08 +0900

    all single-character key test

--- generate.py
+++ generate.py
@@ -167,14 +167,21 @@ write("serialisation-tests/token", tests)
 ### keys
 tests = []
 
-tests.append(
-    {
-        "name": "0x2a for dictionary key",
-        "raw": ["*=1"],
+## single-character dictionary keys
+for c in ALL_CHARS:
+    test = {
+        "name": "0x%02x as a single-character dictionary key" % c,
+        "raw": ["%s=1" % chr(c)],
         "header_type": "dictionary",
-        "expected": [["*",[1,[]]]]
     }
-)
+    if c in WHITESPACE:
+        test["raw"] = ["=1"] # whitespace is always stripped.
+        test["must_fail"] = True
+    elif c in allowed_key_start_chars:
+        test["expected"] = [["%s" % chr(c), [1, []]]]
+    else:
+        test["must_fail"] = True
+    tests.append(test)
 
 ## dictionary keys
 for c in ALL_CHARS:
